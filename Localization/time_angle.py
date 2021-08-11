6af09ad6c871a72eb00b7fd57cfc8fda46edb8cc
Lavan Thaya <46497911+lavanthaya@users.noreply.github.com>
2021-08-11 19:34:40 -0230

    Distance calculation code added and updated

--- Localization/time_angle.py
+++ Localization/time_angle.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# This function takes the time offset reading and convert it into 
-# vertical or horzontal angles of emmited laser sweep.
+# This function takes the time offset reading and calculate the
+# Distance of sensor from origin
 
 import math
 
@@ -8,15 +8,32 @@ import math
 PI = math.pi
 Sin = math.sin
 Cos = math.cos
+Tan = math.tan
+
+omega = PI/(8.33*10**-3) #rad/sec : laser drum angular velocity
+height = 2.4 # lighthouse height in meters
 
 class Time_angle:
     def __init__(self, offset_delta, oriantation):
-    #<time offset in µs> ; <central time offset = 4000> ; <cycle period = 8333>
-        
-        sweep_angle = (offset_delta - 4000) * PI / 8333
+        #offset_delta ==> in micro_sec
+        sweep_angle = omega * (offset_delta * 10**-6) # units in rad
+        angle_degree = math.degrees(sweep_angle)
+
+        if angle_degree < 90:
+            distance = height/Tan(sweep_angle) #pass angles in rad unit inside Tan
+
+        elif angle_degree > 90:
+            distance = height * Tan((PI/2) -sweep_angle) 
+
+        else:
+            distance = 0
+
 
         if (oriantation == 'h'):
-            self.V_hor = [0.0, Cos(sweep_angle), Sin(sweep_angle)]
+            self.hor_pos = distance 
+         
+          
 
         elif (oriantation == 'v'):
-            self.V_ver = [Cos(sweep_angle), 0.0, -Sin(sweep_angle)]
+            self.ver_pos = distance 
+          
